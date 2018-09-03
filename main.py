4b84b7503101ac37dfc8d9822f43d0f944773727
Janos SZABO <jszabo@e3r10p12.42.fr>
2018-09-03 17:55:53 +0200

    starting error checking in lines

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    main.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jszabo <marvin@42.fr>                      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/09/03 17:31:36 by jszabo            #+#    #+#              #
+#    Updated: 2018/09/03 17:31:41 by jszabo           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import sys
 
 def errorQuit(msg):
@@ -17,9 +29,16 @@ def openFile(fname):
 
 def checkRules(line):
     i = 0
+    line = line.split('#', 1)[0]
     while i < len(line) and line[i] != '#':
         print line[i]
-        i = i + 1
+        i += 1
+
+#skip spaces
+#error if there is are multiple characters besides each other
+#error if there are characters that are not 1, uppercase characters
+# 2, parentheses 3, operators
+#error if parentheses don't match
 
 def parseFile(file):
     section = 0
@@ -33,5 +52,4 @@ file = openFile(sys.argv[1])
 parseFile(file)
 
 
-#add 42 header!
 #handling comments and empty files
