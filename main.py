a01a43116f4d73e970ca2d9876780de248697e54
John Szabo <john.szabo.jr@gmail.com>
2018-09-03 17:28:35 +0200

    starting parse

--- main.py
+++ main.py
@@ -15,9 +15,23 @@ def openFile(fname):
     except IOError:
         errorQuit('error: could not read file')
 
+def checkRules(line):
+    i = 0
+    while i < len(line) and line[i] != '#':
+        print line[i]
+        i = i + 1
+
+def parseFile(file):
+    section = 0
+    for line in file:
+        if section == 0:
+            checkRules(line)
+        print len(line)
+
 checkArgs(len(sys.argv))
 file = openFile(sys.argv[1])
-for line in file:
-    print len(line)
+parseFile(file)
 
+
+#add 42 header!
 #handling comments and empty files
